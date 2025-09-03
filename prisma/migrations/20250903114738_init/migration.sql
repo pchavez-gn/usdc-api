45138d07cbc12af4a509816369ed76902b557f33
Herman Paul Chávez <pchavez@globalnexo.com>
2025-09-03 17:22:50 -0400

    fixed error in migration

---
+++ prisma/migrations/20250903114738_init/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "public"."Transfer" (
+    "id" TEXT NOT NULL,
+    "txHash" TEXT NOT NULL,
+    "logIndex" INTEGER NOT NULL,
+    "block" INTEGER NOT NULL,
+    "from" TEXT NOT NULL,
+    "to" TEXT NOT NULL,
+    "amount" TEXT NOT NULL,
+    "timestamp" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Transfer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Transfer_from_idx" ON "public"."Transfer"("from");
+
+-- CreateIndex
+CREATE INDEX "Transfer_to_idx" ON "public"."Transfer"("to");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Transfer_txHash_logIndex_key" ON "public"."Transfer"("txHash", "logIndex");
